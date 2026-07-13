9d4561d2fbaefb604a5d65eb9b9e45939f708696
jeanne <jeannemacycapulong@gmail.com>
2026-07-13 22:51:54 +0800

    s10

--- myApp.js
+++ myApp.js
@@ -36,9 +36,13 @@ app.get("/now", function(req, res, next) {
   res.json({ "time": req.time });
 });
 
-// 9. Get Route Parameter Input from the Client
 app.get("/:word/echo", function(req, res) {
   res.json({ "echo": req.params.word });
+});
+
+// 10. Get Query Parameter Input from the Client
+app.get("/name", function(req, res) {
+  res.json({ "name": req.query.first + " " + req.query.last });
 });
 
 
