ea8ec909cc700752fa8da69dcf8b40ea8874f248
jeanne <jeannemacycapulong@gmail.com>
2026-07-13 22:33:26 +0800

    4

--- myApp.js
+++ myApp.js
@@ -5,7 +5,10 @@ let app = express();
 // 1. Meet the Node console
 console.log("Hello World");
 
-// 3. Serve an HTML File using path.join
+// 4. Serve Static Assets
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// 3. Serve an HTML File
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
