9d505a05be42787f38916011a18dc57f9f7138c0
jeanne <jeannemacycapulong@gmail.com>
2026-07-13 22:35:24 +0800

    5

--- myApp.js
+++ myApp.js
@@ -2,15 +2,17 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-// 1. Meet the Node console
 console.log("Hello World");
 
-// 4. Serve Static Assets (Updated with string path syntax)
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-// 3. Serve an HTML File
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// 5. Serve JSON on a Specific Route
+app.get("/json", function(req, res) {
+  res.json({ "message": "Hello json" });
 });
 
 
