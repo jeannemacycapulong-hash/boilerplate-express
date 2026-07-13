c48f5a3c5c813e248c23c38a2847d106f8bee783
jeanne <jeannemacycapulong@gmail.com>
2026-07-13 22:28:44 +0800

    2

--- myApp.js
+++ myApp.js
@@ -1,3 +1,12 @@
+let express = require('express');
+let app = express();
+
+// 1. Meet the Node console (From previous step)
+console.log("Hello World");
+
+// 2. Start a Working Express Server
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
+
+ module.exports = app;
